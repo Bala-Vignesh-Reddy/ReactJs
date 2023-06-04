808dc39a5a2ea17ced222f10768b069733dc32aa
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-06-04 23:50:12 +0530

    component

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,19 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 3, 12);
-    const expenseTitle = 'Salary';
-    const expenseAmount = 100000;
-    
-    return (
-        <div className='expense-item'>
-            <div>{expenseDate.toISOString}</div>
-            <div className='expense-item__description'> 
-//                 <h2>{expenseTitle}</h2>
-//                 <div className='expense-item__price'>₹{expenseAmount}</div>
-            </div>
-        </div>
-    );
+function ExpenseItem(props) {
+  const expenseDate = new Date(2023, 3, 12);
+  const expenseTitle = 'Salary';
+  const expenseAmount = 100000;
+
+  return (
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>Rs {props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
