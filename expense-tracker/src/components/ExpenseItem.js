34acfd0bea36272fc9a5db6be835711bb9105535
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-06-05 00:06:20 +0530

    component

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,13 +1,23 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2023, 3, 12);
-  const expenseTitle = 'Salary';
-  const expenseAmount = 100000;
+
+  //no use of this because we have used props instead
+  // const expenseDate = new Date(2023, 3, 12);
+  // const expenseTitle = 'Salary';
+  // const expenseAmount = 100000;
+
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { year: '2-digit' });
+  const year = props.date.getFullYear(); 
 
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>Rs {props.amount}</div>
